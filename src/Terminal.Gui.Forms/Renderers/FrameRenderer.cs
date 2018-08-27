67434d41666eddc9381a68d5ee9c522ba2516d97
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-08-27 21:04:53 +0200

    Added Content property into FrameRenderer

--- src/Terminal.Gui.Forms/Renderers/FrameRenderer.cs
+++ src/Terminal.Gui.Forms/Renderers/FrameRenderer.cs
@@ -1,10 +1,13 @@
-﻿using Terminal.Gui.Forms.EventArgs;
+﻿using System.ComponentModel;
+using Terminal.Gui.Forms.EventArgs;
 using Xamarin.Forms;
 
 namespace Terminal.Gui.Forms.Renderers
 {
     public class FrameRenderer : ViewRenderer<Frame, FrameView>
     {
+        VisualElement _currentView;
+
         protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
         {
             base.OnElementChanged(e);
@@ -15,7 +18,24 @@ namespace Terminal.Gui.Forms.Renderers
                 {
                     SetNativeControl(new FrameView(new Rect(), string.Empty));
                 }
+
+                UpdateContent();
             }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == Frame.ContentProperty.PropertyName)
+                UpdateContent();
+        }
+
+        void UpdateContent()
+        {
+            _currentView = Element.Content;
+            var children = _currentView != null ? Platform.GetOrCreateRenderer(_currentView).GetNativeElement() : null;
+            Control.Add(children);
         }
     }
 }
